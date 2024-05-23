611079cfebcdd7fb98d05bbcaedde2ca325c1b4f
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-05-23 12:07:38 -0300

    add shadcn table to use data from it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
-function App() {
-  return <></>;
-}
+import tasks from "./components/data/tasks";
+import { columns } from "./shared/columns";
+import { DataTable } from "./shared/data-table";
 
-export default App;
+export const metadata = {
+  title: "Tasks",
+  description: "A task and issue tracker build using Tanstack Table.",
+};
+
+export default function App() {
+  return (
+    <>
+      <div className="md:hidden">
+        <img
+          src="/examples/tasks-light.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="block dark:hidden"
+        />
+        <img
+          src="/examples/tasks-dark.png"
+          width={1280}
+          height={998}
+          alt="Playground"
+          className="hidden dark:block"
+        />
+      </div>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">{/* <UserNav /> */}</div>
+        </div>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </>
+  );
+}
