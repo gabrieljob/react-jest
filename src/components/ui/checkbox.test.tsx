2f9794e931ab2e5cabfd48b83d9d345c263b1701
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-05-26 11:59:29 -0300

    create checkbox unit tests

---
+++ src/components/ui/checkbox.test.tsx
@@ -0,0 +1,21 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Checkbox } from "./checkbox";
+
+describe("Checkbox", () => {
+  it("should render component using variant", () => {
+    render(<Checkbox />);
+
+    const checkbox = screen.getByTestId("checkbox-testid");
+
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  it("should click on Checkbox", () => {
+    render(<Checkbox />);
+
+    const checkbox = screen.getByTestId("checkbox-testid");
+    fireEvent.click(checkbox);
+
+    expect(checkbox).toBeTruthy();
+  });
+});
