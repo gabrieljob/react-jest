2936717d1b91de1d28f50861ccfe72f767634635
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-05-26 11:16:04 -0300

    create badge unit test

---
+++ src/components/ui/badge.test.tsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import { Badge } from "./badge";
+
+const variants: ("default" | "secondary" | "outline" | "destructive")[] = [
+  "default",
+  "secondary",
+  "outline",
+  "destructive",
+];
+
+describe("Badge", () => {
+  it.each(variants)("hould render component using %s variant", (variant) => {
+    const text = "any_text";
+    render(<Badge variant={variant}>{text}</Badge>);
+
+    const badge = screen.getByTestId("badge-testid");
+
+    expect(badge).toBeInTheDocument();
+  });
+});
