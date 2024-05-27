295b38926867514afb0899747a3ae09ad30c4bf3
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-05-27 13:50:26 -0300

    remove code from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import tasks from "./components/data/tasks";
 import { columns } from "@/shared/columns";
 import { DataTable } from "@/shared/data-table";
-import {
-  CalendarIcon,
-  GearIcon,
-  ReaderIcon,
-  PersonIcon,
-  BellIcon,
-} from "@radix-ui/react-icons";
-import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandInput,
-  CommandItem,
-  CommandList,
-  CommandSeparator,
-} from "./components/ui/command";
 
 export default function App() {
   return (
     <>
-      {/* <div className="md:hidden">
+      <div className="md:hidden">
         <img
           src="/examples/tasks-light.png"
           width={1280}
@@ -47,38 +31,7 @@ export default function App() {
           </div>
         </div>
         <DataTable data={tasks} columns={columns} />
-      </div> */}
-      <Command className="rounded-lg border shadow-md">
-        <CommandInput placeholder="Type a command or search..." />
-        <CommandList>
-          <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Suggestions">
-            <CommandItem>
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              <span>Calendar</span>
-            </CommandItem>
-            <CommandItem>
-              <BellIcon className="mr-2 h-4 w-4" />
-              <span>Notification</span>
-            </CommandItem>
-          </CommandGroup>
-          <CommandSeparator />
-          <CommandGroup heading="Settings">
-            <CommandItem>
-              <PersonIcon className="mr-2 h-4 w-4" />
-              <span>Profile</span>
-            </CommandItem>
-            <CommandItem>
-              <ReaderIcon className="mr-2 h-4 w-4" />
-              <span>Billing</span>
-            </CommandItem>
-            <CommandItem>
-              <GearIcon className="mr-2 h-4 w-4" />
-              <span>Settings</span>
-            </CommandItem>
-          </CommandGroup>
-        </CommandList>
-      </Command>
+      </div>
     </>
   );
 }
