5f0bac64b4af6b9ac78f7b390d47b7e13bc00a90
Gabriel Linck <gabriel.jobc13@gmail.com>
2024-05-28 17:25:37 -0300

    add input unit tests

---
+++ src/components/ui/input.test.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Input } from "./input";
+import userEvent from "@testing-library/user-event";
+
+describe("Input", () => {
+  it("should render component", () => {
+    render(<Input />);
+
+    const input = screen.getByTestId("input-testid");
+
+    expect(input).toBeInTheDocument();
+  });
+  it("should type anything on input type text", async () => {
+    render(<Input />);
+
+    const input = screen.getByTestId("input-testid");
+    await userEvent.type(input, "any_text");
+
+    expect(input).toHaveValue("any_text");
+  });
+  it("should upload a file on input type file", async () => {
+    render(<Input />);
+
+    const input = screen.getByTestId("input-testid") as HTMLInputElement;
+    const file = new File(["any_file"], "any_file.png", {
+      type: "image/png",
+    });
+    Object.defineProperty(input, "files", {
+      value: [file],
+    });
+    fireEvent.change(input);
+    expect(input.files).toHaveLength(1);
+  });
+  it("should input be disabled", async () => {
+    render(<Input disabled />);
+
+    const input = screen.getByTestId("input-testid") as HTMLInputElement;
+    expect(input).toBeDisabled();
+  });
+});
